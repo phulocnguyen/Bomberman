376574bf0cb03640acb4ef293e2a2e538df54189
phulocnguyen <phuloc01.nguyen@gmail.com>
2023-04-07 02:27:13 +0700

    Update Entity.java

---
+++ BombermanGame/src/main/Entities/Entity.java
@@ -0,0 +1,9 @@
+package main.Entities;
+
+public abstract class Entity {
+    // Tọa độ x của vật theo trục ngang
+    protected int x;
+
+    //Tọa độ y của vật the trục thẳng đứng 
+    protected int y;
+}
