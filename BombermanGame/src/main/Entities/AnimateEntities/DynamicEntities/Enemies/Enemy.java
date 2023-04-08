6843a11830f8c8a218ce8e2f12f2b7ba0f8d0454
phulocnguyen <phuloc01.nguyen@gmail.com>
2023-04-09 00:52:38 +0700

    update code

---
+++ BombermanGame/src/main/Entities/AnimateEntities/DynamicEntities/Enemies/Enemy.java
@@ -0,0 +1,9 @@
+package main.Entities.AnimateEntities.DynamicEntities.Enemies;
+
+import javafx.scene.image.Image;
+import main.Entities.AnimateEntities.DynamicEntities.*;
+
+
+public abstract class Enemy extends DynamicEntity {
+    
+}
--- BombermanGame/src/main/Entities/Entity.java
+++ BombermanGame/src/main/Entities/Entity.java
@@ -4,6 +4,9 @@ public abstract class Entity {
     // Tọa độ x của vật theo trục ngang
     protected int x;
 
-    //Tọa độ y của vật the trục thẳng đứng 
+    //Tọa độ y của vật theo trục thẳng đứng 
     protected int y;
+
+    protected boolean invisible;
+
 }
