a2f8988f67d285953971d753783be98fe37bb368
phulocnguyen <phuloc01.nguyen@gmail.com>
2023-04-10 22:14:12 +0700

    update file

---
+++ BombermanGame/src/main/Entities/AnimateEntities/AnimateEntity.java
@@ -0,0 +1,20 @@
+package main.Entities.AnimateEntities;
+
+import main.Entities.Entity;
+import java.math.*;
+
+public abstract class AnimateEntity extends Entity {
+
+    
+
+    public AnimateEntity(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    public double distance(Entity e1, Entity e2) {
+        return Math.sqrt(Math.pow(e1.getX() - e2.getX(), 2) + Math.pow(e1.getY() - e2.getY(), 2));
+    }
+
+
+    
+}
